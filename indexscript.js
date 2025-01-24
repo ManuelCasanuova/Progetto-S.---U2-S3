497d0d9aa718ef12019845e265f8c53fac291710
“manuel <manuelcasanuova@gmail.com>
2025-01-24 10:00:57 +0100

    .

---
+++ indexscript.js
@@ -0,0 +1,17 @@
+const URL = `https://striveschool-api.herokuapp.com/api/product/`;
+
+fetch(URL, {
+  method: "GET",
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzNTEyY2I3NDcwMTAwMTU4YjJhYzUiLCJpYXQiOjE3Mzc3MDc4MjAsImV4cCI6MTczODkxNzQyMH0.6xr86aB2GYQ89d5Q2JRGZDx9V2FMSz1GTclfEH7ed-w",
+  },
+})
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error();
+    }
+  })
+  .then((pexelsData) => {});
