73fd782ed56e25d66a3fea5b542b508c6a81294f
“manuel <manuelcasanuova@gmail.com>
2025-01-24 16:00:48 +0100

    .

--- Script/backofficescript.js
+++ Script/backofficescript.js
@@ -25,20 +25,21 @@ window.addEventListener("DOMContentLoaded", () => {
         Authorization:
           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzNTEyY2I3NDcwMTAwMTU4YjJhYzUiLCJpYXQiOjE3Mzc3MDc4MjAsImV4cCI6MTczODkxNzQyMH0.6xr86aB2GYQ89d5Q2JRGZDx9V2FMSz1GTclfEH7ed-w",
       },
-    });
-    then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Errore durante il recupero del prodotto");
-      }
-    }).then((product) => {
-      form.elements.name.value = product.name;
-      form.elements.description.value = product.description;
-      form.elements.brand.value = product.brand;
-      form.elements.url.value = product.imageUrl;
-      form.elements.price.value = product.price;
-    });
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Errore durante il recupero del prodotto");
+        }
+      })
+      .then((product) => {
+        form.elements.name.value = product.name;
+        form.elements.description.value = product.description;
+        form.elements.brand.value = product.brand;
+        form.elements.url.value = product.imageUrl;
+        form.elements.price.value = product.price;
+      });
   } else {
     submitBtn.innerText = "Aggiungi prodotto";
     submitBtn.classList.add("btn-warning", "mb-5");
