15dabf94f77abfdb7bb9a839c6a6aad885b528bf
“manuel <manuelcasanuova@gmail.com>
2025-01-24 11:54:06 +0100

    .

---
+++ backofficescript.js
@@ -0,0 +1,117 @@
+const form = document.getElementById("product-form");
+
+const params = new URLSearchParams(window.location.search);
+const productId = params.get("appId");
+const URL = productId
+  ? "https://striveschool-api.herokuapp.com/api/product/" + productId
+  : "https://striveschool-api.herokuapp.com/api/product/";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const submitBtn = document.getElementById("submit-btn");
+  const subtitle = document.querySelector("h2 + h5");
+  const delBtn = document.getElementById("delete-btn");
+
+  if (productId) {
+    submitBtn.innerText = "Modifica prodotto";
+    submitBtn.classList.add("btn-success");
+    subtitle.innerText = "— Modifica prodotto";
+
+    delBtn.classList.remove("d-none");
+    delBtn.onclick = handleDelete;
+
+    fetch(URL);
+    then((resp) => {
+      if (resp.ok) {
+        return resp.json();
+      } else {
+        throw new Error("Errore durante il recupero del prodotto");
+      }
+    }).then((product) => {
+      //console.log(product);
+
+      form.elements.name.value = product.name;
+      form.elements.description.value = product.description;
+      form.elements.brand.value = product.brand;
+      form.elements.url.value = product.imageUrl;
+      form.elements.price.value = product.price;
+    });
+  } else {
+    submitBtn.innerText = "Aggiungi prodotto";
+    submitBtn.classList.add("btn-primary");
+    subtitle.innerText = "— Crea nuovo prodotto";
+  }
+});
+
+form.onsubmit = function (event) {
+  event.preventDefault();
+
+  const newProduct = {
+    name: form.elements.name.value,
+    description: form.elements.description.value,
+    brand: form.elements.brand.value,
+    imageUrl: form.elements.url.value,
+    price: parseFloat(form.elements.price.value),
+  };
+
+  if (
+    !newProduct.name ||
+    !newProduct.description ||
+    !newProduct.brand ||
+    !newProduct.imageUrl ||
+    isNaN(newProduct.price)
+  ) {
+    generateAlert("Compila correttamente tutti i campi del form!");
+    return;
+  }
+
+  fetch(URL, {
+    method: productId ? "PUT" : "POST",
+    body: JSON.stringify(newProduct),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzNTEyY2I3NDcwMTAwMTU4YjJhYzUiLCJpYXQiOjE3Mzc3MDc4MjAsImV4cCI6MTczODkxNzQyMH0.6xr86aB2GYQ89d5Q2JRGZDx9V2FMSz1GTclfEH7ed-w",
+    },
+  })
+    .then((resp) => {
+      if (resp.ok) {
+        return resp.json();
+      } else {
+        throw new Error("Errore nella creazione del prodotto, verificare di aver compilato il form correttamente");
+      }
+    })
+    .then((createProd) => {
+      if (!productId) {
+        alert("Il prodotto " + createProd._id + " creato correttamente, lo potrai visualizzare nella home");
+
+        form.reset();
+      } else {
+        alert("Il prodotto " + createProd._id + " modificato correttamente!");
+      }
+    });
+};
+
+const handleDelete = () => {
+  const hasConfirmed = confirm("sei sicuro di voler eliminare questo prodotto?");
+
+  if (hasConfirmed) {
+    fetch(URL, {
+      method: "DELETE",
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzNTEyY2I3NDcwMTAwMTU4YjJhYzUiLCJpYXQiOjE3Mzc3MDc4MjAsImV4cCI6MTczODkxNzQyMH0.6xr86aB2GYQ89d5Q2JRGZDx9V2FMSz1GTclfEH7ed-w",
+      },
+    })
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        }
+      })
+      .then((deletedProd) => {
+        alert("Il prodotto" + deletedProd.name + "è stato eliminato correttamente");
+
+        window.location.assign("./index.html");
+      })
+      .catch((err) => console.log(err));
+  }
+};
--- indexscript.js
+++ indexscript.js
@@ -15,7 +15,53 @@ fetch(URL, {
     }
   })
   .then((productsData) => {
-    productsData.array.forEach((product) => {
+    const row = document.getElementById("products-grid");
+
+    productsData.forEach((product) => {
       const col = document.createElement("div");
+      col.classList.add("col");
+      const card = document.createElement("div");
+      card.classList.add("card");
+      const cardBody = document.createElement("div");
+      cardBody.classList.add("card-body");
+      const img = document.createElement("img");
+      img.src = product.imageUrl;
+      img.alt = product.name;
+      img.classList.add("bd-placeholder-img", "card-img-top");
+      const title = document.createElement("h5");
+      title.classList.add("card-title");
+      title.innerText = product.name;
+      const cardText = document.createElement("p");
+      cardText.classList.add("card-text");
+      cardText.innerText = product.price;
+      const btnContainer = document.createElement("div");
+      btnContainer.classList.add("d-flex", "justify-content-between", "align-items-center");
+      const btnGroup = document.createElement("div");
+      btnGroup.classList.add("btn-group");
+      const modifyBtn = document.createElement("button");
+      modifyBtn.type = "button";
+      modifyBtn.classList.add("btn", "btn-sm", "btn btn-primary");
+      modifyBtn.innerText = "Modifica prodotto";
+      const detailsBtn = document.createElement("button");
+      detailsBtn.type = "button";
+      detailsBtn.classList.add("btn", "btn-sm", "btn-outline-primary");
+      detailsBtn.innerText = "Dettagli prodotto";
+
+      row.appendChild(col);
+      col.appendChild(card);
+      card.appendChild(img);
+      card.appendChild(cardBody);
+      cardBody.appendChild(title);
+      cardBody.appendChild(cardText);
+      cardBody.appendChild(btnContainer);
+      btnContainer.appendChild(btnGroup);
+      btnGroup.appendChild(detailsBtn);
+      btnGroup.appendChild(modifyBtn);
     });
+  })
+  .catch((err) => {
+    console.dir(err);
+    // isLoading(false);
+
+    generateAlert(err.message);
   });
